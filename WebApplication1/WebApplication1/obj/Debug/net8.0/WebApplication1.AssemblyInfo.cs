79763e5b068a5733e0306d651ce4357c9d073446
tahsindincel <dnclthsn@gmail.com>
2024-08-24 13:28:01 +0300

    Delete and Add
    bazı dosyalar silindi başkaları oluşturuldu

--- WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+195ff00b7811e7bd0b976697ce8f4c177643de31")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b6b88c8fb85e9364584fc0989251cd47f0ac731")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
